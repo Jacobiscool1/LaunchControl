1afb17ba6405b39837d0ad47329dd54c8f404ee3
Jacob Rutigliano <jao2156132@maricopa.edu>
2023-03-21 15:15:49 -0700

    week8

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,5 +1,5 @@
 showTime();
-var gasLevel = 0;
+var gasLevel = 0, park = 0, traction = 0 , battery = 0;
 
 function getGas() {
     if (gasLevel == 0){
@@ -11,5 +11,41 @@ function getGas() {
         gasLevel = 0;
         document.getElementById('bttn_gas').src='./assets/images/bttn_gas_0.PNG';
         document.getElementById('gas').src='./assets/images/gas0.png';
+    }
+}
+function parkBrake(){
+    if (park == 0){
+        park= 1;
+        document.getElementById('bttn_park').src='./assets/images/bttn_park_1.PNG';
+        document.getElementById('brake').src='./assets/images/p1.PNG';
+    }
+    else {
+        park= 0;
+        document.getElementById('bttn_park').src='./assets/images/bttn_park_0.PNG';
+        document.getElementById('brake').src='./assets/images/p0.PNG';
+    }
+}
+function tractionControl() {
+    if (traction == 0){
+        traction = 1;
+        document.getElementById('bttn_trac').src='/assets/images/bttn_trac_1.PNG';
+        document.getElementById('trac').src='./assets/images/trac0.PNG';
+    }
+    else {
+        traction = 0;
+        document.getElementById('bttn_trac').src='/assets/images/bttn_trac_0.PNG';
+        document.getElementById('trac').src='./assets/images/trac1.PNG';
+    }
+}
+function chargeBattery() {
+    if (battery == 0){
+        battery = 1;
+        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_0.PNG';
+        document.getElementById('batt').src='./assets/images/batt0.PNG';
+    }
+    else {
+        battery = 0;
+        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_1.PNG';
+        document.getElementById('batt').src='./assets/images/batt1.PNG';
     }
 }
