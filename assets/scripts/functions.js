2c1f99beebf603db373918e82b42762fd86e28bf
Jacob Rutigliano <jao2156132@maricopa.edu>
2023-03-28 13:52:32 -0700

    launchcontrol1

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,5 +1,5 @@
 showTime();
-var gasLevel = 0, park = 0, traction = 0 , battery = 0;
+var gasLevel = 0, park = 1, traction = 1 , battery = 0,  intMode = 0 ;
 
 function getGas() {
     if (gasLevel == 0){
@@ -28,24 +28,51 @@ function parkBrake(){
 function tractionControl() {
     if (traction == 0){
         traction = 1;
-        document.getElementById('bttn_trac').src='/assets/images/bttn_trac_1.PNG';
-        document.getElementById('trac').src='./assets/images/trac0.PNG';
+        document.getElementById('bttn_trac').src='./assets/images/bttn_trac1.PNG';
+        document.getElementById('trac').src='./assets/images/trac1.PNG';
     }
     else {
         traction = 0;
-        document.getElementById('bttn_trac').src='/assets/images/bttn_trac_0.PNG';
-        document.getElementById('trac').src='./assets/images/trac1.PNG';
+        document.getElementById('bttn_trac').src='./assets/images/bttn_trac_0.PNG';
+        document.getElementById('trac').src='./assets/images/trac0.PNG';
     }
 }
 function chargeBattery() {
     if (battery == 0){
         battery = 1;
-        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_0.PNG';
-        document.getElementById('batt').src='./assets/images/batt0.PNG';
+        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_1.PNG';
+        document.getElementById('batt').src='./assets/images/batt1.PNG';
     }
     else {
         battery = 0;
-        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_1.PNG';
-        document.getElementById('batt').src='./assets/images/batt1.PNG';
+        document.getElementById('bttn_batt').src='./assets/images/bttn_batt_0.PNG';
+        document.getElementById('batt').src='./assets/images/batt0.PNG';
+    }
+}
+function setMode(){
+    var status = document.getElementById('mode');
+    var shifter = document.getElementById('bttn_shifter');
+    switch(intMode){
+        case 0:
+            intMode = 1;
+            shifter.style.top = '725px';
+            status.innerText = 'ECO PRO';
+            break;
+        case 1:
+            intMode = 2;
+            shifter.style.top = '750px';
+            status.innerText = 'COMFORT';
+            break;
+        case 2:
+            intMode = 3;
+            shifter.style.top = '775px';
+            status.innerText = 'SPORT';
+            break;
+        default:
+            intMode = 4;
+            shifter.style.top = '700px';
+            status.innerText = 'PARK'
+            break;
+    
     }
 }
